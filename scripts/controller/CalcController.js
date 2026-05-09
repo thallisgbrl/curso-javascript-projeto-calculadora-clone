a89d732ccf9f88a557a4c6a8213e777c6179f1fe
Thallis <thallisgabriel66@gmail.com>
2026-05-09 18:47:32 -0300

    C05 C06 C07 C08 C09 - Data e hora, querySelector, Adicionando Evento Click, Aplicando vários Eventos usando split(), Switch

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,34 +2,173 @@ class CalcController {
 
     constructor() {
 
+        this._operation = [];
+        this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
     }
 
     initialize() {
 
+        this.setDisplayDateTime();
 
-        this._dateEl.innerHTML = "01/05/2020";
-        this._timeEl.innerHTML = "00:00";
+        setInterval(()=> {
+
+            this.setDisplayDateTime();
+
+        }, 1000);
+    }
+
+    addEventListener(element, events, fn) {
+
+            events.split(' ').forEach(event => {
+
+                element.addEventListener(event, fn, false);
+
+            });
 
     }
+
+    clearAll(){
+
+        this._operation = [];
+
+    }
+
+    cancelEntry(){
+
+        this._operation.pop();
+
+    }
+
+    addOperation(value){
+
+        this._operation.push(value);
+
+        console.log(this._operation);
+
+    }
+
+
+    setError(){
+
+        this.displayCalc = "Error";
+
+    }
+
+    execBtn(value) {
+
+            console.log(value);
+        switch (value) {
+
+            case 'ac':
+                this.clearAll;
+                break;
+
+            case 'ce':
+                this.cancelEntry;
+                break;
+
+            case 'soma':
+                
+                break;
+
+            case 'subtracao':
+                
+                break;
+
+            case 'divisao':
+                
+                break;
+
+            case 'multiplicacao':
+                
+                break;
+
+            case 'porcento':
+                
+                break;
+
+            case 'igual':
+                
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+        }
+
+    }
+
+
+    initButtonsEvents() {
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=>{
+
+           this.addEventListener(btn, "click drag", e => {
+
+                let textBtn = btn.className.baseVal.replace("btn-","");
+                
+                this.execBtn(textBtn);
+
+            });
+
+            this.addEventListener(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer";
+
+            });
+
+        });
+
+    }
+
+
+    setDisplayDateTime() {
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day:"2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+    }
+
     get displayTime() {
-    return this._timeEl.innerHTML;
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        return this._timeEl.innerHTML = value;
+    }    
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
     }
 
     set displayDate(value) {
-        return this._timeEl.innerHTML = value;
-    }
-    get displayTime() {
-    return this._dateEl.innerHTML;
-    }
-
-    get displayDate() {
-        return this._timeEl.innerHTML;
+        return this._dateEl.innerHTML = value;
     }
 
     get displayCalc() {
@@ -44,7 +183,7 @@ class CalcController {
         return new Date();
     }
 
-    set currentDate(data) {
-        this._currentDate = data; 
+    set currentDate(value) {
+        this._currentDate = value; 
     }
 }
